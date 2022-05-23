f4548bf6dff0ec41e0e4977ba3d110ffc902c653
junseok <njs702@ajou.ac.kr>
2022-05-23 11:15:10 +0900

    온/습도 센서 모듈 DHT-11 사용해서 측정하기, 함수 모듈화

--- Arduino/source.ino
+++ Arduino/source.ino
@@ -1,9 +1,31 @@
+#include <dht.h>
+
+dht DHT;
+
+/* PIN NUMBER settings */
+int DHT_11_PIN = 3;
+
+/* struct data stores temp & humid info */
+typedef struct{
+    float humid; 
+    float temp;
+}temp_humid_data;
+
+static temp_humid_data temp_humid;
+
+/* read temperature & humid from DHT_11 module */
+void read_temp_humid(){
+    DHT.read11(DHT_11_PIN);
+    temp_humid.humid = DHT.humidity;
+    temp_humid.temp = DHT.temperature;
+}
+
 void setup()
 {
-	
+	Serial.begin(115200);
 }
 
 void loop()
 {
-	
+	read_temp_humid();
 }
