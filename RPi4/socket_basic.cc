a6a146c683bd45e584aa5e1498cc0ce757339686
junseok <njs702@ajou.ac.kr>
2022-05-27 14:05:48 +0900

    소켓 통신용 구조체 및 유니온 선언, 소켓 정보 전역변수 선언

--- RPi4/socket_basic.cc
+++ RPi4/socket_basic.cc
@@ -21,10 +21,42 @@
 
 #include <signal.h>
 
+
+
 /* ============== MACRO DEFINE ============== */
 #define PORT 9000
 #define IP "192.168.0.80"
 /* ========================================== */
+
+
+
+/* ============== GLOBAL custom struct & union ============== */
+typedef struct{
+	float humid;
+	float temp; 
+}temp_humid_data;
+
+union temp_humid_union {
+    temp_humid_data first;
+    unsigned char second[8];
+}; 
+/* ========================================================== */
+
+
+
+/* ============== GLOBAL variables ============== */
+static int server_socket; // TCP socket
+static struct sockaddr_in server_addr, client_addr;
+
+static temp_humid_data temp_humid;
+
+static int can_socket; // CAN socket
+static struct sockaddr_can addr;
+static struct ifreq ifr; // ifreq structure contaning the interface name
+/* ============================================== */
+
+
+
 
 int main(){
     
