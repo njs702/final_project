ec82f4b13cb9e0929564c38f7b605e9cbeabf19a
junseok <njs702@ajou.ac.kr>
2022-05-27 14:00:08 +0900

    RPi 소켓 통신 구현을 위한 헤더 파일 선언, 매크로 선언

--- RPi4/socket_basic.cc
+++ RPi4/socket_basic.cc
@@ -1,4 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <fcntl.h> // 파일 처리를 위한 헤더파일
+#include <unistd.h> // POSIX 운영체제 API 엑세스 제공 헤더파일
+
+#include <pthread.h> // 쓰레드 헤더파일
+#include <arpa/inet.h> // hton, ntoh , htons, ... etc
+
+#include <sys/socket.h> // socket, bind, listen, ...
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h> // sockaddr_in 
+#include <net/if.h>
+
+#include <stdint.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include <signal.h>
+
+/* ============== MACRO DEFINE ============== */
+#define PORT 9000
+#define IP "192.168.0.80"
+/* ========================================== */
 
 int main(){
     
