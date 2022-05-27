a59dc767a9159a38cf5badb8b19dfe193e0dce7e
junseok <njs702@ajou.ac.kr>
2022-05-27 14:09:45 +0900

    소켓 생성 및 bind 함수 작성

--- RPi4/socket_basic.cc
+++ RPi4/socket_basic.cc
@@ -57,7 +57,35 @@ static struct ifreq ifr; // ifreq structure contaning the interface name
 
 
 
+/* ============== GLOBAL functions ============== */
+void init_bind_socket_tcp(){
+
+    // 서버를 위한 소켓 생성
+    // PF_INET - 프로토콜 체계(프로토콜 패밀리), SOCK_STREAM - 전송 타입(연결 지향형 소켓->tcp), IPPROTO_TCP - 프로토콜(연결 지향형)
+    server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(server_socket == -1){
+        perror("Socket()");
+        return;
+    }
+
+    // 입력받는 포트 번호를 사용해 서비스를 운영체제에 등록
+    memset(&server_addr,0,sizeof(server_addr));
+    server_addr.sin_family = AF_INET; // AF_INET - 주소 체계(주소 패밀리)
+    server_addr.sin_addr.s_addr = inet_addr(IP);
+    //server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(PORT);
+
+    // 소켓 bind
+    if(bind(server_socket,(struct sockaddr*)&server_addr,sizeof(server_addr))==-1){
+        printf("bind()");
+        return;
+    }
+    printf("TCP Socket creation & bind success!\n");
+} // TCP 소켓 생성 및 바인드 함수
+/* ============================================== */
+
+
 
 int main(){
-    
+    init_bind_socket_tcp();
 }
